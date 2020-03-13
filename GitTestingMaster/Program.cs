df9e948826522954aef87f9e0c30a1be03c5d80f
Evo-Tim <2894+Evo-Tim@users.noreply.github.com>
2020-03-13 09:38:38 +0000

    c# file setup
    file setup, and print testing line

---
+++ GitTestingMaster/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestingMaster
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("This should be in the master string");
+		}
+	}
+}
